6f536f3e3a33326ee1f25f5b310267e447d2bb45
Discoinferno <007gnail@gmail.com>
2024-10-16 09:28:53 +0100

    compose tint bar animations with Metal optimisation

--- Sources/ASlider/1.SliderViews/Tint.swift
+++ Sources/ASlider/1.SliderViews/Tint.swift
@@ -48,6 +48,7 @@ struct Tint: View {
                     color: sliderStyle.trackShadowColor,
                     radius: sliderStyle.trackShadowRadius
                 )
+                .drawingGroup() // compose animations with Metal optimisation
             }
         } 
     }
